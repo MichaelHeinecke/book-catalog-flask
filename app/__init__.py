baa6ab3faf7816aee2dc1ef59cf0cc17aec27991
Michael Heinecke <michael.heinecke@booking.com>
2021-11-10 16:21:36 +0100

    Add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
 def create_app(config: str):
@@ -12,7 +14,8 @@ def create_app(config: str):
     configuration = Path.joinpath(Path.cwd(), 'config', config + '.py')
     application.config.from_pyfile(configuration)
 
-    db.init_app(application)
+    db.init_app(application)  # bind database to flask application
+    bootstrap.init_app(application)
 
     from app.catalog import main
     application.register_blueprint(main)
