1d8fa2188d97788b88f8998c2800e7640ac47c6d
Michael Heinecke <michael.heinecke@booking.com>
2021-11-10 15:14:46 +0100

    Remove code commented out

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ def create_app(config: str):
     """Create instance of Flask application with config for dev, test, or prod"""
     application = Flask(__name__)
     configuration = Path.joinpath(Path.cwd(), 'config', config + '.py')
-    # configuration = os.path.join(os.getcwd(), 'config', config + '.py')
     application.config.from_pyfile(configuration)
 
     db.init_app(application)
