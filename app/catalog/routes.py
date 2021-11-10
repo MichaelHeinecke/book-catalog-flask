157dae9e6aa56568904822d871eb022dc10ce485
Michael Heinecke <michael.heinecke@booking.com>
2021-11-10 16:06:31 +0100

    Add route to display publisher on publisher.html and link from home.html

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template
 
 from app.catalog import main
-from app import db
 from app.catalog.models import Book, Publisher
 
 
@@ -9,3 +8,10 @@ from app.catalog.models import Book, Publisher
 def display_books():
     book_list = Book.query.all()
     return render_template('home.html', books=book_list)
+
+
+@main.route('/display/publisher/<int:publisher_id>')
+def display_publisher(publisher_id: int):
+    publisher = Publisher.query.get(publisher_id)
+    publisher_books = Book.query.filter_by(pub_id=publisher.id).all()
+    return render_template('publisher.html', publisher=publisher, publisher_books=publisher_books)
