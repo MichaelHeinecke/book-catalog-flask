4322a5c1e6c5e75249a96bbfab47ebe849579ad4
Michael Heinecke <michael.heinecke@booking.com>
2021-11-15 17:12:30 +0100

    Add password field and validators to RegistrationForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Enter your name')
-    email = StringField('Enter your email')
+    name = StringField('Enter your name', validators=[DataRequired(),
+                                                      Length(3, 15, 'must be between 3 and 15 characters')])
+    email = StringField('Enter your email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 15, 'must be between 8 and 15 '
+                                                                                   'characters'),
+                                                     EqualTo('confirm', message='passwords must match')])
+    confirm = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
