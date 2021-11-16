4cbc05678a01908adcc37c2cd171e2a70880fd23
Michael Heinecke <michael.heinecke@booking.com>
2021-11-16 15:27:02 +0100

    Improve registration route: invoke validators and create user record for POST requests

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,17 +1,21 @@
-from flask import render_template, request
+from flask import render_template, flash, url_for, redirect
 
 from app.auth import authentication
 from app.auth.forms import RegistrationForm
+from app.auth.models import User
 
 
 @authentication.route('/register', methods=['GET', 'POST'])
 def register_user():
-    name = None
-    email = None
     form = RegistrationForm()
 
-    if request.method == 'POST':
-        name = form.name.data
-        email = form.email.data
+    if form.validate_on_submit():  # check if POST request and invoke validators specified in form
+        User.create_user(
+            user=form.name.data,
+            email=form.name.data,
+            password=form.password.data
+        )
+        flash('Registration successful')
+        return redirect(url_for('at.login_user'))
 
-    return render_template('registration.html', form=form, name=name, email=email)
+    return render_template('registration.html', form=form)
